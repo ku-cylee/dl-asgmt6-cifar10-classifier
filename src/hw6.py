3dc7285dcfbb14d0856f78e8447e6c7a81ec3432
Chanyoung Lee <btstar_cy@korea.ac.kr>
2021-12-19 16:06:01 +0900

    Rough implementation

--- src/hw6.py
+++ src/hw6.py
@@ -19,6 +19,93 @@ class IdentityResNet(nn.Module):
     # nblk_stage1: number of blocks in stage 1, nblk_stage2.. similar
     def __init__(self, nblk_stage1, nblk_stage2, nblk_stage3, nblk_stage4):
         super(IdentityResNet, self).__init__()
+
+        self.nblk_stage1 = nblk_stage1
+        self.nblk_stage2 = nblk_stage2
+        self.nblk_stage3 = nblk_stage3
+        self.nblk_stage4 = nblk_stage4
+
+        self.conv = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1)
+
+        # Stage 1
+        self.stage1_blk = nn.Sequential(*[
+            nn.BatchNorm2d(num_features=64),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1),
+        ] * 2)
+
+        # Stage 2
+        self.stage2_firstblk_pre = nn.Sequential(
+            nn.BatchNorm2d(num_features=64),
+            nn.ReLU(),
+        )
+
+        self.stage2_firstblk_unskip = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(num_features=128),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
+        )
+
+        self.stage2_firstblk_skip = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=1, stride=2),
+        )
+
+        self.stage2_elseblk = nn.Sequential(*[
+            nn.BatchNorm2d(num_features=128),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
+        ] * 2)
+
+        # Stage 3
+        self.stage3_firstblk_pre = nn.Sequential(
+            nn.BatchNorm2d(num_features=128),
+            nn.ReLU(),
+        )
+
+        self.stage3_firstblk_unskip = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(num_features=256),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1),
+        )
+
+        self.stage3_firstblk_skip = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=1, stride=2),
+        )
+
+        self.stage3_elseblk = nn.Sequential(*[
+            nn.BatchNorm2d(num_features=256),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1),
+        ] * 2)
+
+        # Stage 4
+        self.stage4_firstblk_pre = nn.Sequential(
+            nn.BatchNorm2d(num_features=256),
+            nn.ReLU(),
+        )
+
+        self.stage4_firstblk_unskip = nn.Sequential(
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(num_features=512),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
+        )
+
+        self.stage4_firstblk_skip = nn.Sequential(
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=1, stride=2),
+        )
+
+        self.stage4_elseblk = nn.Sequential(*[
+            nn.BatchNorm2d(num_features=512),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
+        ] * 2)
+
+        self.avg_pooling = nn.AvgPool2d(kernel_size=4, stride=4)
+        self.fc = nn.Linear(512, 10)
+
     ########################################
     # Implement the network
     # You can declare whatever variables
@@ -33,6 +120,22 @@ class IdentityResNet(nn.Module):
         # Implement the network
         # You can declare or define whatever variables or methods
         ########################################
+
+        stg1_out = self.stage1_blk(self.stage1_blk(self.conv(x)))
+
+        stg2_pre = self.stage2_firstblk_pre(stg1_out)
+        stg2_first = self.stage2_firstblk_unskip(stg2_pre) + self.stage2_firstblk_skip(stg2_pre)
+        stg2_out = self.stage2_elseblk(stg2_first) + stg2_first
+
+        stg3_pre = self.stage3_firstblk_pre(stg2_out)
+        stg3_first = self.stage3_firstblk_unskip(stg3_pre) + self.stage3_firstblk_skip(stg3_pre)
+        stg3_out = self.stage3_elseblk(stg3_first) + stg3_first
+
+        stg4_pre = self.stage4_firstblk_pre(stg3_out)
+        stg4_first = self.stage4_firstblk_unskip(stg4_pre) + self.stage4_firstblk_skip(stg4_pre)
+        stg4_out = self.stage4_elseblk(stg4_first) + stg4_first
+        
+        out = self.fc(self.avg_pooling(stg4_out).view(-1, 512))
         return out
 
 ########################################
@@ -41,7 +144,7 @@ class IdentityResNet(nn.Module):
 # If available, set dev to "cuda:0";
 # otherwise set dev to "cpu"
 ########################################
-dev = ...
+dev = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 print('current device: ', dev)
 
 
@@ -53,7 +156,7 @@ print('current device: ', dev)
 # Q2. set batch size
 # set batch size for training data
 ########################################
-batch_size = ...
+batch_size = 8
 
 # preprocessing
 transform = transforms.Compose([transforms.ToTensor(),
@@ -83,8 +186,7 @@ net = IdentityResNet(nblk_stage1=2, nblk_stage2=2,
 # Q3. load model to GPU
 # Complete below to load model to GPU
 ########################################
-...
-
+model = net.to(dev)
 
 # set loss function
 criterion = nn.CrossEntropyLoss()
@@ -94,7 +196,7 @@ criterion = nn.CrossEntropyLoss()
 # Complete below to use SGD with momentum (alpha= 0.9)
 # set proper learning rate
 ########################################
-optimizer = ...
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-2, momentum=.9)
 
 # start training
 t_start = time.time()
@@ -110,12 +212,12 @@ for epoch in range(5):  # loop over the dataset multiple times
         # Q5. make sure gradients are zero!
         # zero the parameter gradients
         ########################################
-        ...
+        model.zero_grad()
         
         ########################################
         # Q6. perform forward pass
         ########################################
-        outputs = ...
+        outputs = model.forward(inputs)
         
         # set loss
         loss = criterion(outputs, labels)
@@ -123,12 +225,12 @@ for epoch in range(5):  # loop over the dataset multiple times
         ########################################
         # Q7. perform backprop
         ########################################
-        ...
+        loss.backward()
         
         ########################################
         # Q8. take a SGD step
         ########################################
-        ...
+        optimizer.step()
         
         # print statistics
         running_loss += loss.item()
@@ -151,7 +253,7 @@ class_total = list(0. for i in range(10))
 # Q9. complete below
 # when testing, computation is done without building graphs
 ########################################
-with ...
+with torch.no_grad():
     for data in testloader:
         images, labels = data[0].to(dev), data[1].to(dev)
         outputs = net(images)
